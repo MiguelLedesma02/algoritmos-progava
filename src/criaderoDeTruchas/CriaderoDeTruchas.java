ba1568fad6c77bfead4f99f6f182c1261796bb09
leo99 <leo99@192.168.0.219>
2024-05-19 23:50:17 -0300

    trabajado de llenar

--- src/criaderoDeTruchas/CriaderoDeTruchas.java
+++ src/criaderoDeTruchas/CriaderoDeTruchas.java
@@ -8,47 +8,92 @@ public class CriaderoDeTruchas {
 
 	private Vertedero vertedero;
 	private List<Estanque> estanques;
-	
-	CriaderoDeTruchas(List<Estanque> estanques){
-		
+
+	CriaderoDeTruchas(List<Estanque> estanques) {
+
 		this.estanques = new ArrayList<Estanque>();
 		try {
 			this.estanques = estanques;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	private void asignarCañerias() {
+
+		//ver cañeria derecha en ultimo estanque
+		// nos fijamos si es el ultimo por el indice y en cañeriaDer = 0?
 		
 		int profundidadCañeriaIzq = 0;
-		for(Estanque estanque : this.estanques) {	
-			
+		for (Estanque estanque : this.estanques) {
+
 			estanque.setProfundidadCañeriaIzq(profundidadCañeriaIzq);
-			profundidadCañeriaIzq = estanque.getProfundidadCañeriaIzq();
+			profundidadCañeriaIzq = estanque.getProfundidadCañeriaDer();
 		}
+
 	}
-	
+
 	public void llenar() {
-		
-		this.asignarCañerias();
-		
-		for(Estanque estanque : this.estanques) {	
-			
-			if(!estanque.estaLleno() && this.vertedero.getVolumenDeAgua() > 0) {
-				
-				int cañeriaMasBaja = estanque.getCañeriaMasBaja();
-				if(cañeriaMasBaja == estanque.getProfundidadCañeriaDer()) {
-					//Te vas a la der
-					
-					
-				}
-				else {
-					//Te vas a la izq
-				}
-				
-			}
+
+		Archivo archivo = new Archivo();
+
+		int maximaCapacidadEstanques = 0;
+
+		for (Estanque estanque : this.estanques) {
+			maximaCapacidadEstanques += estanque.getVolumenMaximo();
 		}
+
+		if (maximaCapacidadEstanques <= vertedero.getVolumenDeAgua()) { // veo si hay desborde
+			this.asignarCañerias();
+
+			Estanque estanque = estanques.get(vertedero.getUbicacion());
+
+			while (vertedero.getVolumenDeAgua() > 0) {
+
+				if (!estanque.estaLleno()) {
+
+					int cañeriaMasBaja = estanque.getCañeriaMasBaja();
+					if (cañeriaMasBaja == estanque.getProfundidadCañeriaDer()) {
+						// Te vas a la der
+						int volumenCargado = 0;
+						if (vertedero.getVolumenDeAgua() >= estanque.getVolumenMaximo()) {
+							volumenCargado = (estanque.getProfundidad() - estanque.getProfundidadCañeriaDer())
+									* estanque.getSuperficie();
+						} else {
+							volumenCargado = vertedero.getVolumenDeAgua();
+						}
+
+						estanque.setVolumenCargado(volumenCargado);
+						vertedero.setVolumenDeAgua(vertedero.getVolumenDeAgua() - volumenCargado);
+
+						int indice = estanques.indexOf(estanque);
+						estanque = estanques.get(indice + 1);
+
+					} else {
+						// Te vas a la izq
+						int volumenCargado = 0;
+						if (vertedero.getVolumenDeAgua() >= estanque.getVolumenMaximo()) {
+							volumenCargado = (estanque.getProfundidad() - estanque.getProfundidadCañeriaIzq())
+									* estanque.getSuperficie();
+						} else {
+							volumenCargado = vertedero.getVolumenDeAgua();
+						}
+
+						estanque.setVolumenCargado(volumenCargado);
+						vertedero.setVolumenDeAgua(vertedero.getVolumenDeAgua() - volumenCargado);
+
+						int indice = estanques.indexOf(estanque);
+						estanque = estanques.get(indice - 1);
+					}
+
+				}
+			}
+		} else {
+			int volumenDesborde = maximaCapacidadEstanques - vertedero.getVolumenDeAgua();
+			archivo.generarArchivoSalidaDesborde(volumenDesborde);
+
+		}
+
 	}
 }
--- src/criaderoDeTruchas/Estanque.java
+++ src/criaderoDeTruchas/Estanque.java
@@ -4,17 +4,17 @@ public class Estanque {
 	private int numeroEstanque;
 	private int superficie;
 	private int profundidad;
-	private int volumenMaximo = superficie * profundidad;
+	private int volumenMaximo;
 	private int volumenCargado;
 	private int profundidadCañeriaIzq;
 	private int profundidadCañeriaDer;
 	private boolean tieneVertedero;
-	//private Estanque anterior;
-	//private Estanque siguiente;
-	
-	
+	private int profundidadCargada = volumenCargado / superficie;
+	// private Estanque anterior;
+	// private Estanque siguiente;
+
 	public Estanque(int numeroEstanque, int superficie, int profundidad, int profundidadCañeriaDer) {
-		
+
 		this.numeroEstanque = numeroEstanque;
 		this.superficie = superficie;
 		this.profundidad = profundidad;
@@ -23,11 +23,11 @@ public class Estanque {
 		this.profundidadCañeriaDer = profundidadCañeriaDer;
 		this.tieneVertedero = false;
 	}
-		
+
 	public int getNumeroEstanque() {
 		return numeroEstanque;
 	}
-	
+
 	public int getVolumenMaximo() {
 		return volumenMaximo;
 	}
@@ -39,35 +39,45 @@ public class Estanque {
 	public void setVolumenCargado(int volumenCargado) {
 		this.volumenCargado = volumenCargado;
 	}
-	
+
 	public int getProfundidadCañeriaIzq() {
 		return profundidadCañeriaIzq;
 	}
-	
+
 	public void setProfundidadCañeriaIzq(int profundidadCañeriaIzq) {
-		 this.profundidadCañeriaIzq = profundidadCañeriaIzq;
+		this.profundidadCañeriaIzq = profundidadCañeriaIzq;
 	}
 
 	public int getProfundidadCañeriaDer() {
 		return profundidadCañeriaDer;
 	}
+	
+	public int getProfundidad() {
+		return profundidad;
+	}
+	
+	public int getSuperficie() {
+		return superficie;
+	}
 
 	public boolean estaLleno() {
-		
+
 		return this.volumenMaximo == this.volumenCargado;
 	}
-	
+
 	public boolean cañeriaAlcanzada() {
-	
-		return this.profundidadCañeriaDer == this.volumenCargado/this.superficie;
+
+		return this.profundidadCañeriaDer == this.volumenCargado / this.superficie;
 	}
-	
+
 	public int getCañeriaMasBaja() {
-		
-		if(this.profundidadCañeriaIzq < this.profundidadCañeriaDer)
+
+		if (this.profundidadCañeriaIzq > this.profundidadCañeriaDer && this.profundidadCañeriaIzq > this.profundidadCargada) // viendo desde arriba, la mas profunda va a ser la cañeria mas baja desde el agua
 			return this.profundidadCañeriaIzq;
-		
+
 		return this.profundidadCañeriaDer;
+		
+		//ver ultimo estanque para retornar la cañeria mas baja que seria la izquierda
 	}
-	
+
 }
