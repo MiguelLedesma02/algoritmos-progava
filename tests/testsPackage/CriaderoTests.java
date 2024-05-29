0637451120e80cf2b5ebfd8d9ded05f6154e5835
Rodrigo Berti <rodriberti98@gmail.com>
2024-05-29 18:46:09 -0300

    agregar test n estanques

--- tests/testsPackage/CriaderoTests.java
+++ tests/testsPackage/CriaderoTests.java
@@ -6,7 +6,7 @@ import criaderoDeTruchas.*;
 import java.util.*;
 
 class CriaderoTests {
-
+    
 	@Test
 	void unEstanqueConSuficienteProfundidadAsignaNivelDeProfundidad() {
 
@@ -87,6 +87,54 @@ class CriaderoTests {
 		// Assert
 		int nivelDeCargaEsperado = 0; // vacío, no pudo llenar nada
 		assertEquals(estanques.get(0).getNivel(), nivelDeCargaEsperado);
+	}
+	
+	@Test
+	void nEstanquesSeLlenanSinDesbordarPuntasCriadero() {
+		// Arrange
+				int numero1 = 1;
+				int numero2= 2;
+				int numero3= 3;
+				int numero4= 4;
+				int proCañoDer1 = 4;
+				int proCañoDer2= 2;
+				int proCañoDer3= 1;
+				int proCañoDer4= 0;
+				int superficie1 = 120;
+				int superficie2= 80;
+				int superficie3= 60;
+				int superficie4= 50;
+				int profundidad1 = 7;
+				int profundidad2= 10;
+				int profundidad3= 5;
+				int profundidad4= 3;
+				Estanque estanque = new Estanque(numero1, superficie1, profundidad1, proCañoDer1);
+				Estanque estanque2 = new Estanque(numero2, superficie2, profundidad2, proCañoDer2);
+				Estanque estanque3 = new Estanque(numero2, superficie2, profundidad2, proCañoDer2);
+				Estanque estanque4 = new Estanque(numero2, superficie2, profundidad2, proCañoDer2);
+
+				List<Estanque> estanques = new ArrayList<Estanque>();
+				estanques.add(estanque);
+				estanques.add(estanque2);
+				estanques.add(estanque3);
+				estanques.add(estanque4);
+
+				int volumenDeAguaPorCargar = 1040;
+				int numeroDeEstanque = 2;
+				Vertedero vertedero = new Vertedero(volumenDeAguaPorCargar, numeroDeEstanque);
+
+				Criadero criadero = new Criadero(estanques, vertedero);
+
+				// Act
+				criadero.llenar();
+
+				// Assert
+				int nivelDeCargaEsperado = 4;
+				System.out.println(nivelDeCargaEsperado);
+				assertEquals(estanques.get(0).getNivel(), nivelDeCargaEsperado);
+				nivelDeCargaEsperado = 7;
+				System.out.println(nivelDeCargaEsperado);
+				assertEquals(estanques.get(1).getNivel(), nivelDeCargaEsperado);
 	}
 	
 	
