1abfff6f90e69fb16337303be0808331f12c9a10
Rodrigo Berti <rodriberti98@gmail.com>
2024-06-12 15:43:41 -0300

    Update Mina.java

--- src/programacion_dinamica/Mina.java
+++ src/programacion_dinamica/Mina.java
@@ -9,45 +9,50 @@ public class Mina {
 				     {8, 5, 12, 32, 21},
 				     {3, 20, 2, 9, 7}};
 		
-		System.out.println(robarMina(m));
+		System.out.println(minaDeOro(m));
 
 	}
 
-	public static int robarMina(int[][] m) {
-		
-		int[][] memo = m;
-		int f = m.length;
-		int c = m[0].length;
-		
-		//La primera fila es fija
-		for(int i = 1; i < f; i ++) {
-			
-			for(int j = 0; j < c; j ++) {
-				
-				//Estoy en la izq
-				if(j == 0)
-					memo[i][j] = m[i][j] + Math.max(memo[i-1][j], memo[i-1][j+1]);
-				
-				//Estoy por el medio
-				if(j > 0 && j < c-1) 
-					memo[i][j] = m[i][j] + Math.max(memo[i-1][j], 
-							     	       Math.max(memo[i-1][j-1], memo[i-1][j+1]));
-				
-				//Estoy en la der
-				if(j == c-1)
-					memo[i][j] = m[i][j] + Math.max(memo[i-1][j-1], memo[i-1][j]);
+
+		public static int minaDeOro(int[][] mina) {
+			int res = 0;
+
+			int fila = mina.length;
+			int columna = mina[0].length;
+
+			for (int i = 1; i < fila; i++) {
+				for (int j = 0; j < columna; j++) {
+					if (i - 1 >= 0 && j - 1 >= 0 && j + 1 < columna) {
+
+						int mayorIzq = mina[i - 1][j] > mina[i - 1][j - 1] ? mina[i - 1][j] : mina[i - 1][j - 1];
+						int mayorDer = mina[i - 1][j] > mina[i - 1][j + 1] ? mina[i - 1][j] : mina[i - 1][j + 1];
+
+						int mayor = mina[i][j] + (mayorIzq > mayorDer ? mayorIzq : mayorDer);
+
+						mina[i][j] = mayor;
+
+					} else if (i - 1 >= 0 && j == 0) {
+
+						int mayor = mina[i][j]
+								+ (mina[i - 1][j] > mina[i - 1][j + 1] ? mina[i - 1][j] : mina[i - 1][j + 1]);
+
+						mina[i][j] = mayor;
+					} else if (i - 1 >= 0 && j == columna - 1) {
+						int mayor = mina[i][j]
+								+ (mina[i - 1][j] > mina[i - 1][j - 1] ? mina[i - 1][j] : mina[i - 1][j - 1]);
+						mina[i][j] = mayor;
+					}
+				}
 			}
+
+			// encuentro mayor de la ultima fila
+			res = mina[fila - 1][0];
+			for (int i = 1; i < columna; i++) {
+				if (mina[fila - 1][i] > res) {
+					res = mina[fila - 1][i];
+				}
+			}
+
+			return res;
 		}
-		
-		int botin = 0;
-		
-		//Obtengo el maximo de la ultima fila
-		for(int j = 0; j < c; j ++) {
-			if(memo[f-1][j] > botin)
-				botin = memo[f-1][j];
-		}
-		
-		return botin;
 	}
-	
-}
