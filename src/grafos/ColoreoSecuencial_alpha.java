641c920d8178962aedacd8b52f8f61fdaa6efd09
Rodrigo Berti <rodriberti98@gmail.com>
2024-06-28 17:56:49 -0300

    nuevo coloreoSecuencial

--- src/grafos/ColoreoSecuencial_alpha.java
+++ src/grafos/ColoreoSecuencial_alpha.java
@@ -6,69 +6,57 @@ import java.util.Arrays;
 
 public class ColoreoSecuencial_alpha {
 
-	private static int cantVertices;
-	private int[][] matrizAdyacencia;
+    
+    public static int[] coloreoSecuencial(int[][] grafo) {
+        int n = grafo.length;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);  // -1 indica que el vértice aún no ha sido coloreado
 
-	public ColoreoSecuencial_alpha(int[][] matriz) {
-		matrizAdyacencia = matriz;
-		cantVertices = matriz.length;
-	}
+        // Colorear cada vértice secuencialmente
+        for (int vertice = 0; vertice < n; vertice++) {
+            boolean[] coloresDisponibles = new boolean[n];
+            Arrays.fill(coloresDisponibles, true);  // Todos los colores están inicialmente disponibles
 
-	public int[] colorear() {
+            // Marcar colores de los vértices adyacentes como no disponibles
+            for (int adyacente = 0; adyacente < n; adyacente++) {
+                if (grafo[vertice][adyacente] == 1 && color[adyacente] != -1) {
+                    coloresDisponibles[color[adyacente]] = false;
+                }
+            }
 
-		int[] colorVertice = new int[this.cantVertices];
-		boolean[] verticeDisponible = new boolean[cantVertices];
+            // Encontrar el primer color disponible
+            int colorAElegir;
+            for (colorAElegir = 0; colorAElegir < n; colorAElegir++) {
+                if (coloresDisponibles[colorAElegir]) {
+                    break;
+                }
+            }
 
-		Arrays.fill(colorVertice, -1);
-		Arrays.fill(verticeDisponible, true);
+            // Asignar el color al vértice
+            color[vertice] = colorAElegir;
+        }
 
-		colorVertice[0] = 0; //
+        return color;
+    }
 
-		for (int i = 1; i < cantVertices; i++) {
-			for (int j = 0; j < cantVertices; j++) {
-				if (matrizAdyacencia[i][j] == 1 && colorVertice[j] != -1) {
-					verticeDisponible[colorVertice[j]] = false;
-				}
-			}
+    public static void main(String[] args) {
+        int[][] graph = {
+            { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+            { 1, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 1, 1, 0, 0, 0 },
+            { 0, 0, 0, 0, 1, 0, 0, 1, 0, 0 },
+            { 0, 1, 0, 1, 0, 0, 0, 0, 1, 0 },
+            { 1, 0, 1, 0, 0, 0, 1, 0, 1, 1 },
+            { 0, 0, 1, 0, 0, 1, 0, 0, 0, 1 },
+            { 0, 0, 0, 1, 0, 0, 0, 0, 1, 0 },
+            { 0, 0, 0, 0, 1, 1, 0, 1, 0, 1 },
+            { 0, 0, 0, 0, 0, 1, 1, 0, 1, 0 }
+        };
 
-			int color;
-			for (color = 0; color < cantVertices; color++) {
-				if (verticeDisponible[color]) {
-					break;
-				}
-			}
-			colorVertice[i] = color;
-			Arrays.fill(verticeDisponible, true);
-
-		}
-		return colorVertice;
-	}
-
-	private static void imprimir(int[] color) {
-		System.out.println("Resultado de Coloreo de Vértices:");
-		for (int i = 0; i < cantVertices; i++)
-			System.out.println("Vértice " + i + " --->  Color " + color[i]);
-	}
-
-	public static void main(String[] args) {
-
-		int[][] matriz = {
-				// A;B;C;D;E;F;G;H;I;J
-				{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 }, // A
-				{ 1, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, // B
-				{ 0, 0, 0, 0, 0, 1, 1, 0, 0, 0 }, // C
-				{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0 }, // D
-				{ 0, 1, 0, 1, 0, 0, 0, 0, 1, 0 }, // E
-				{ 1, 0, 1, 0, 0, 0, 1, 0, 1, 1 }, // F
-				{ 0, 0, 1, 0, 0, 1, 0, 0, 0, 1 }, // G
-				{ 0, 0, 0, 1, 0, 0, 0, 0, 1, 0 }, // H
-				{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 1 }, // I
-				{ 0, 0, 0, 0, 0, 1, 1, 0, 1, 0 } // J
-		};
-
-		ColoreoSecuencial_alpha grafo = new ColoreoSecuencial_alpha(matriz);
-		int[] coloreo = grafo.colorear();
-		imprimir(coloreo);
-	}
+        int[] coloreo = coloreoSecuencial(graph);
+        for (int i = 0; i < coloreo.length; i++) {
+            System.out.println("Vertice " + i + " --->  Color " + coloreo[i]);
+        }
+    }
 
 }
